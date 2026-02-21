9accd69c6275c585848ff5d240b252673a6ed3f0
jmgallol <jmgallol@eafit.edu.co>
2026-02-21 12:59:45 -0500

    Archivos actualizados tutorial 2

--- laravelcourse/resources/views/product/show.blade.php
+++ laravelcourse/resources/views/product/show.blade.php
@@ -12,9 +12,15 @@
         <h5 class="card-title">
            {{ $viewData["product"]["name"] }}
         </h5>
-        <p class="card-text">{{ $viewData["product"]["description"] }}</p>
+        <p class="card-text">{{ $viewData["product"]["price"] }}</p>
+
+        @foreach($viewData["product"]->comments as $comment)
+          - {{ $comment->getDescription() }}<br />
+        @endforeach
+
       </div>
     </div>
   </div>
 </div>
 @endsection
+
